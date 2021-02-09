099946b34ba73df931d374abe212f78bdff987d2
早川飛希 <otinnotoki@superMacBook-Air.local>
2021-02-10 00:47:15 +0900

    cdn読み込めない

--- public/Browine.js
+++ public/Browine.js
@@ -79,7 +79,7 @@ socket.on("join-result", (data)=>{
     }
     //プッシュ通知の許可
     Push.Permission.request();
-    
+
     // 表示を切り替える
     $("#inputmyname").style.display = "none";   // 名前入力を非表示
     $("#chat").style.display = "block";         // チャットを表示
